62a4df786e8361243dd36d8310bd51f565081d3b
JP-Ellis <josh@jpellis.me>
2018-05-07 11:14:14 +1000

    Improve implementation of polynomial function
    It now allows for empty lists in which case it returns the constant `0.0`.
    
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- src/polynomial.rs
+++ src/polynomial.rs
@@ -1,26 +1,24 @@
-//! Polynomials
-//!
-//! Functions to deal with polynomials.
+//! Utilities to handle polynomials
 
+/// Evaluates an arbitrary single-variable polynomial at a particular point.
+///
 /// Given a list of coefficient \\(c = [c_0, c_1, \dots, c_n]\\), evaluates the
 /// polynomial
 /// \\[
 ///     p(x) = c_0 + c_1 x + c_2 x\^2 + \dots + c_n x\^n
 /// \\]
 /// at the specified value of \\(x\\).
+///
+/// If the list of coefficients is empty, the function returns `0.0`.
+///
+/// # Warning
+///
+/// This function does not perform any checks on the coefficients.
 #[inline]
 pub fn polynomial(x: f64, c: &[f64]) -> f64 {
-    debug_assert!(
-        !c.is_empty(),
-        "List of coefficients must have at least one element."
-    );
-
     // This expands the polynomial as:
     //
     // c0 + x * (c1 + x * (c2 + x * (...)))
-    let n = c.len();
-    c.iter()
-        .rev()
-        .skip(1)
-        .fold(c[n - 1], |ans, &ci| ci + x * ans)
+    c.iter().rev().fold(0.0, |ans, &ci| ci + x * ans)
+}
 }
