be7ba648835ef8ab07b07bad28f81ffc341e9a28
JP-Ellis <josh@jpellis.me>
2018-05-07 11:14:44 +1000

    Add tests and benchmarks for polynomial.rs
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- src/polynomial.rs
+++ src/polynomial.rs
@@ -21,4 +21,85 @@ pub fn polynomial(x: f64, c: &[f64]) -> f64 {
     // c0 + x * (c1 + x * (c2 + x * (...)))
     c.iter().rev().fold(0.0, |ans, &ci| ci + x * ans)
 }
+
+#[cfg(test)]
+mod test {
+    use utilities::test::*;
+
+    pub(crate) const COEFFICIENTS: [f64; 9] = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0];
+
+    #[test]
+    fn len_0() {
+        for x in -100..100 {
+            let x = x as f64;
+            approx_eq(super::polynomial(x, &COEFFICIENTS[..0]), 0.0, 14.0, 0.0);
+        }
+    }
+
+    #[test]
+    fn len_1() {
+        for x in -100..100 {
+            let x = x as f64;
+            approx_eq(super::polynomial(x, &COEFFICIENTS[..1]), 1.0, 14.0, 0.0);
+        }
+    }
+
+    #[test]
+    fn len_2() {
+        for x in -100..100 {
+            let x = x as f64;
+            approx_eq(
+                super::polynomial(x, &COEFFICIENTS[..2]),
+                1.0 + 2.0 * x,
+                14.0,
+                0.0,
+            );
+        }
+    }
+
+    #[test]
+    fn len_3() {
+        for x in -100..100 {
+            let x = x as f64;
+            approx_eq(
+                super::polynomial(x, &COEFFICIENTS[..3]),
+                1.0 + 2.0 * x + 3.0 * x.powi(2),
+                14.0,
+                0.0,
+            );
+        }
+    }
+
+    #[test]
+    fn len_9() {
+        for x in -100..100 {
+            let x = x as f64;
+            approx_eq(
+                super::polynomial(x, &COEFFICIENTS),
+                1.0 + 2.0 * x + 3.0 * x.powi(2) + 4.0 * x.powi(3) + 5.0 * x.powi(4)
+                    + 6.0 * x.powi(5) + 7.0 * x.powi(6) + 8.0 * x.powi(7)
+                    + 9.0 * x.powi(8),
+                14.0,
+                0.0,
+            );
+        }
+    }
+}
+
+#[cfg(feature = "nightly")]
+#[cfg(test)]
+mod bench {
+    use test::{black_box, Bencher};
+
+    use super::test::COEFFICIENTS;
+
+    #[bench]
+    fn len_9(b: &mut Bencher) {
+        b.iter(|| {
+            for x in -100..100 {
+                let x = x as f64;
+                black_box(super::polynomial(x, &COEFFICIENTS));
+            }
+        })
+    }
 }
