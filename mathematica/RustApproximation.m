c2fdbcfee80bd9384ebaa49e11dc0cec71268e45
JP-Ellis <josh@jpellis.me>
2019-07-21 19:44:47 +0800

    Show the current interval
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- mathematica/RustApproximation.m
+++ mathematica/RustApproximation.m
@@ -133,7 +133,7 @@ PiecewiseApproximate[f_, {x_, x0_, x1_}, OptionsPattern[]] := Module[
        AppendTo[xs, {xStart, xi}];
        xStart = xi;
        xi = Min[xEnd, xStart + 2 * (xs[[-1, 2]] - xs[[-1, 1]])];
-       Print[StringTemplate["`` Intervals; `` :: ``"][Length[xs], N[xStart, 4], N[xEnd, 4]]];
+       Print[StringTemplate["`` Intervals; [``, ``] :: ``"][Length[xs], N[xStart, 4], N[xi, 4], N[xEnd, 4]]];
      ,
        xi = (xStart + xi)/2;
     ];
