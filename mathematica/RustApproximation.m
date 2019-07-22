fce01aa333c1952e7784615c42e2433681bc3779
JP-Ellis <josh@jpellis.me>
2019-07-22 17:13:10 +0800

    Add a PiecewiseInteprolation
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- mathematica/RustApproximation.m
+++ mathematica/RustApproximation.m
@@ -1,9 +1,6 @@
 (* ::Package:: *)
 
-
 <<FunctionApproximations`;
-
-$MaxExtraPrecision = 1000;
 
 Options[BestMiniMax] = {
   "TargetError" -> $MachineEpsilon / 100,
@@ -65,12 +62,11 @@ BestMiniMax[f_, {x_, x0_, x1_}, opts : OptionsPattern[]] := Module[
 
 Protect[BestMiniMax];
 
-(* ::Input::Initialization:: *)
 Options[PiecewiseMiniMax] = {
   "TargetError" -> $MachineEpsilon / 100,
   "MaxOrder" -> 10,
-  "MaxNumeratorOrder" -> 4,
-  "MaxDenominatorOrder" -> 0,
+  "MaxNumeratorOrder" -> Automatic,
+  "MaxDenominatorOrder" -> Automatic,
   WorkingPrecision -> 200,
   "StartGuess" -> Automatic,
   "EndGuess" -> Automatic
@@ -127,7 +123,7 @@ PiecewiseMiniMax[f_, {x_, x0_, x1_}, opts : OptionsPattern[]] := Module[
          ];
   AppendTo[xs, {xEnd, x1}];
   AppendTo[approxes, approx];
-  Print[StringTemplate["Lower approximation valid from `` to ``."][N[xEnd, 4], N[x1, 4]]];
+  Print[StringTemplate["Upper approximation valid from `` to ``."][N[xEnd, 4], N[x1, 4]]];
 
   (* Now subdivide the remaining (inner) interval until the local errors on each
   is sufficiently small *)
@@ -162,6 +158,160 @@ PiecewiseMiniMax[f_, {x_, x0_, x1_}, opts : OptionsPattern[]] := Module[
 ];
 Protect[PiecewiseMiniMax];
 
+
+Options[PiecewiseInterpolation] = {
+  "TargetError" -> $MachineEpsilon / 100,
+  Order -> 10,
+  WorkingPrecision -> 200,
+  "StartGuess" -> Automatic,
+  "EndGuess" -> Automatic
+};
+
+PiecewiseInterpolation[f_, {x_, x0_, x1_}, opts : OptionsPattern[]] := Module[
+  {
+    approxes={}, approx, xs={},
+    xStart, xEnd,
+    xi, err=\[Infinity],
+    startGuess, endGuess,
+    tmp
+  },
+
+  PrintTemporary[
+    Dynamic@Row[
+      {
+        StringTemplate["`` subdivisions"][Length[approxes]],
+        ProgressIndicator[xStart, {xs[[1, 2]], xs[[2, 1]]}],
+        N[xs, 3]
+      },
+      "  "]
+  ];
+
+  (* Find the points in the interval where a usual series no longer work, at
+  both the start and the end. *)
+  approx = Normal@Series[f, {x, x0, OptionValue[Order]}];
+  startGuess = If[
+    OptionValue["StartGuess"] === Automatic,
+    (99 * x0 + x1) / 100,
+    OptionValue["StartGuess"]
+  ];
+  xStart = x /. FindRoot[
+    ((approx - f) / f)^2 == SetPrecision[OptionValue["TargetError"], \[Infinity]]^2,
+    {x, startGuess, x0, x1},
+    WorkingPrecision -> 2 * OptionValue[WorkingPrecision],
+    MaxIterations -> Infinity
+           ];
+  AppendTo[xs, {x0, xStart}];
+  AppendTo[approxes, approx];
+  Print[StringTemplate["Lower approximation valid from `` to ``."][N[x0, 4], N[xStart, 4]]];
+
+  approx = Normal@Series[f, {x, x1, OptionValue[Order]}];
+  endGuess = If[
+    OptionValue["EndGuess"] === Automatic,
+    (99 * x1 + x0) / 100,
+    OptionValue["EndGuess"]
+  ];
+  xEnd = x /. FindRoot[
+    ((approx - f) / f)^2 == SetPrecision[OptionValue["TargetError"], \[Infinity]]^2,
+    {x, endGuess, x0, x1},
+    WorkingPrecision -> 2 * OptionValue[WorkingPrecision],
+    MaxIterations -> Infinity
+         ];
+  AppendTo[xs, {xEnd, x1}];
+  AppendTo[approxes, approx];
+  Print[StringTemplate["Upper approximation valid from `` to ``."][N[xEnd, 4], N[x1, 4]]];
+
+  (* Now subdivide the remaining (inner) interval until the local errors on each
+  is sufficiently small *)
+  PrintTemporary[
+    Dynamic@StringTemplate["Current Interval = [``, ``]"][
+      N[xStart, 4],
+      N[xi, 4]]
+  ];
+  Print[StringTemplate["`` Intervals; `` :: ``"][Length[xs], N[xStart, 4], N[xEnd, 4]]];
+  xi = xEnd;
+  While[
+    xStart < xEnd,
+    approx = InterpolatingPolynomial[Table[{x, f}, {x, Subdivide[xStart, xi, OptionValue[Order]]}], x];
+    {approx, err} = BestMiniMax[
+      f, {x, xStart, xi},
+      FilterRules[{opts}, Options[BestMiniMax]]];
+    If[err < OptionValue["TargetError"],
+       AppendTo[approxes, approx];
+       AppendTo[xs, {xStart, xi}];
+       xStart = xi;
+       xi = Min[xEnd, xStart + 2 * (xs[[-1, 2]] - xs[[-1, 1]])];
+       Print[StringTemplate["`` Intervals; [``, ``] :: ``"][Length[xs], N[xStart, 4], N[xi, 4], N[xEnd, 4]]];
+     ,
+       xi = (xStart + xi)/2;
+    ];
+  ];
+
+  tmp = Transpose[{approxes, #1 <= x <#2 & @@@ xs}];
+  Function[
+    Evaluate[{x}],
+    Evaluate@Piecewise[tmp]
+  ]
+];
+Protect[PiecewiseInterpolation];
+
+
+ClearAll[ApproximationToRust];
+ApproximationToRust[f_Function, out_OutputStream] := Module[
+  {
+    x,approxes,tmp,
+    splits,numerators,denominators
+  },
+
+  (* Deconstruct the Piecewise function *)
+  x = f[[1,1]];
+  tmp = f[[2,1]];
+  tmp = tmp /. {a___, {___, False}, b___} :> {a, b};
+  tmp = SortBy[tmp, #[[2,1]]&];
+  numerators = CoefficientList[Numerator[tmp[[;;, 1]]], x] /. {0} -> {};
+  denominators = CoefficientList[Denominator[tmp[[;;, 1]]], x] /. {1} -> {};
+  splits = DeleteDuplicates@Sort@Flatten[{tmp[[;;, 2, 1]], tmp[[;;, 2, -1]]}];
+
+  (* Write out the numerators *)
+  WriteString[
+    out,
+    StringTemplate["pub const NUMERATORS: [&[f64]; ``] = [\n"][Length@numerators]
+  ];
+  Do[
+    WriteString[
+      out,
+      StringTemplate["    &``,\n"][ToRustList[row]]];
+  ,
+    {row,numerators}
+  ];
+  WriteString[out,"];\n\n"];
+
+  (* Write out the denominators *)
+  WriteString[
+    out,
+    StringTemplate["pub const DENOMINATORS: [&[f64]; ``] = [\n"][Length@denominators]
+  ];
+  Do[
+    WriteString[
+      out,
+      StringTemplate["    &``,\n"][ToRustList[row]]
+    ];
+  ,
+    {row,denominators}
+  ];
+  WriteString[out,"];\n\n"];
+
+  (* Write the splits *)
+  WriteString[
+    out,
+    StringTemplate["pub const SPLITS: [f64; ``] = ``;"][
+      Length@splits,
+      StringReplace[ToRustList[splits], {
+        "DirectedInfinity(-1)" -> "std::f64::NEG_INFINITY",
+        "DirectedInfinity(1)" -> "std::f64::INFINITY"
+                    }]
+  ]];
+];
+Protect[PiecewiseMiniMax];
 
 (* ::Input::Initialization:: *)
 ClearAll[ApproximationToRust];
@@ -221,6 +371,8 @@ ApproximationToRust[f_Function, out_OutputStream] := Module[
   ]];
 ];
 Protect[PiecewiseMiniMax];
+
+
 
 ToRustList[l_List] := "[" <> StringRiffle[
   ToString@CForm@N[#] & /@ l,
