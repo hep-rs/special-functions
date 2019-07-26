b5ab7bbe6abf99a3e33abaa8a7f7330b2f7facdc
JP-Ellis <josh@jpellis.me>
2019-07-26 11:21:54 +0800

    Add k1_on_k2
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- mathematica/BesselK.wl
+++ mathematica/BesselK.wl
@@ -99,3 +99,85 @@ use crate::approximations::polynomial;\n\n"
 ,
   {n, Range[0, 9]}
 ];
+
+
+Print["Approximating BesselK[1, x] / BesselK[2, x]"];
+f[x_] := BesselK[1, x] / BesselK[2, x];
+output = OpenWrite[FileNameJoin[{
+  Directory[],
+  "../src/bessel/k1_on_k2.rs"
+  }]];
+
+WriteString[
+  output,
+  "#![allow(clippy::all)]
+
+use crate::approximations::polynomial;\n\n"
+];
+
+(* Find the series approximation for small x *)
+lower = Normal@Series[f[x], {x, 0, 10}];
+xLower = x / 50 /. FindRoot[
+  Abs[lower / f[x] - 1] == SetPrecision[$MachineEpsilon, Infinity],
+  {x, 2, 0, Infinity},
+  WorkingPrecision -> 5 $MachinePrecision,
+  MaxIterations -> Infinity
+];
+Print[StringTemplate["Lower approximation valid from `` to ``."][0, N[xLower, 4]]];
+
+(* Write out the approximation valid for small x. *)
+{lowerPoly, lowerLn, lowerLn2} = CoefficientList[x^-1 lower, Log[x]];
+lowerPoly = CoefficientList[lowerPoly, x^2];
+lowerLn = CoefficientList[x^-2 lowerLn, x^2];
+lowerLn2 = CoefficientList[x^-6 lowerLn2, x^2];
+WriteString[
+  output,
+  StringTemplate["pub fn lower(x: f64) -> f64 {
+    let x2 = x.powi(2);
+    let x6 = x2.powi(3);
+    let ln = x.ln();
+    let ln2 = ln.powi(2);
+
+    x * (
+        polynomial(x2, &`lowerPoly`)
+        + x2 * ln * polynomial(x2, &`lowerLn`)
+        + x6 * ln2 * polynomial(x2, &`lowerLn2`)
+    )
+}\n\n"][<|
+  "lowerPoly" -> ToRustList@lowerPoly,
+  "lowerLn" -> ToRustList@lowerLn,
+  "lowerLn2" -> ToRustList@lowerLn2
+  |>]
+];
+
+
+(* Find the series approximation for large x *)
+upper = Normal@Series[f[x], {x, Infinity, 10}];
+xUpper = x /. FindRoot[
+  Abs[upper / f[x] - 1] == SetPrecision[$MachineEpsilon, Infinity],
+  {x, 2, 0, Infinity},
+  WorkingPrecision -> 5 $MachinePrecision,
+  MaxIterations -> Infinity
+        ];
+Print[StringTemplate["Upper approximation valid from `` to ``."][N[xUpper, 4], Infinity]];
+
+(* Write out the approximation valid for large x. *)
+upper = CoefficientList[upper, 1/x];
+WriteString[
+  output,
+  StringTemplate["pub fn upper(x: f64) -> f64 {
+    polynomial(x.recip(), &`upper`)
+}\n\n"][<|
+  "upper" -> ToRustList@upper
+  |>]
+];
+
+
+(* Subdivide the remaining interval using Chebyshev polynomials *)
+splits = ChebyshevSplits[
+  f[x], {x, xLower, xUpper},
+  PrecisionGoal -> 2 $MachinePrecision,
+  AccuracyGoal -> Min[10^(-2 $MachinePrecision), GeometricMean[f[{xLower, xUpper}]]]];
+ChebyshevSplitsToRust[splits, output];
+
+Close[output];
--- src/bessel.rs
+++ src/bessel.rs
@@ -4,6 +4,7 @@ use approx_fn;
 
 mod k0;
 mod k1;
+mod k1_on_k2;
 mod k2;
 mod k3;
 mod k4;
@@ -56,6 +57,11 @@ approx_fn! {
 approx_fn! {
    #[doc = "Approximation of modified Bessel function \\(K_9(x)\\) for all \\(x > 0\\)."]
    (pub) fn k9(mod = k9, type = chebyshev);
+}
+
+approx_fn! {
+    #[doc = "Approximation of the ratio of modified Bessel function \\(K_1(x) / K_2(x)\\) for all \\(x > 0\\)."]
+    (pub) fn k1_on_k2(mod = k1_on_k2, type = chebyshev);
 }
 
 #[cfg(test)]
@@ -203,21 +209,20 @@ mod tests {
         }
     }
 
-    // #[test]
-    // fn k1_on_k2() {
-    //     let mut rdr = csv::Reader::from_path("tests/data/bessel_k1_on_k2.csv").unwrap();
+    #[test]
+    fn k1_on_k2() {
+        let mut rdr = csv::Reader::from_path("tests/data/bessel/k1_on_k2.csv").unwrap();
 
-    //     for result in rdr.deserialize() {
-    //         let (x, v): (f64, f64) = result.unwrap();
+        for result in rdr.deserialize() {
+            let (x, v): (f64, f64) = result.unwrap();
 
-    //         if !v.is_nan() {
-    //             let n = super::k1_on_k2(x);
-    //             approx_eq(n, v, 11.0, 0.0);
-    //         }
-    //     }
-
-    //     assert_eq!(super::k1_on_k2(0.0), 0.0);
-    // }
+            if !v.is_nan() {
+                println!("x0 = {:e}", x);
+                let n = super::k1_on_k2(x);
+                approx_eq(n, v, 10.0, 0.0);
+            }
+        }
+    }
 }
 
 #[cfg(feature = "nightly")]
@@ -375,18 +380,18 @@ mod bench {
         });
     }
 
-    // #[bench]
-    // fn k1_on_k2(b: &mut Bencher) {
-    //     let rdr = csv::Reader::from_path("tests/data/bessel_k1_on_k2.csv").unwrap();
-    //     let data: Vec<(f64, f64)> = rdr.into_deserialize().map(|x| x.unwrap()).collect();
+    #[bench]
+    fn k1_on_k2(b: &mut Bencher) {
+        let rdr = csv::Reader::from_path("tests/data/bessel/k1_on_k2.csv").unwrap();
+        let data: Vec<(f64, f64)> = rdr.into_deserialize().map(|x| x.unwrap()).collect();
 
-    //     b.iter(|| {
-    //         for &(x, v) in &data {
-    //             if !v.is_nan() {
-    //                 let n = super::k1_on_k2(x);
-    //                 test::black_box((x, v, n));
-    //             }
-    //         }
-    //     });
-    // }
+        b.iter(|| {
+            for &(x, v) in &data {
+                if !v.is_nan() {
+                    let n = super::k1_on_k2(x);
+                    test::black_box((x, v, n));
+                }
+            }
+        });
+    }
 }
